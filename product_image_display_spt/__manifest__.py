1cdd6f18ecd94109cb5f0cfd4f9a2bb3c5cdc701
savanh-keypress <savanh.keypress@gmail.com>
2024-12-27 10:02:50 +0000

    ddeess

--- product_image_display_spt/__manifest__.py
+++ product_image_display_spt/__manifest__.py
@@ -12,4 +12,4 @@
 'data': ['security/ir.model.access.csv', 'views/sale_order_view.xml', 'views/purchase_order_view.xml', 'views/stock_picking_view.xml', 'reports/sale_order_extend_report.xml', 'reports/purchase_quotation_extend_report.xml', 'reports/purchase_order_extend_report.xml', 'reports/product_report_action.xml', 'reports/product_template_report_template_spt.xml', 'reports/product_product_report_template_spt.xml', 'reports/stock_picking_extend.xml', 'reports/delivery_slip_report_extend.xml'],
 'application': True,
 'auto_install': False,
-'live_test_url': 'https://youtu.be/I4wfFCc7NEU',}
+'live_test_url': 'https://www.youtube.com/watch?v=-zwE9DhFCmA',}
