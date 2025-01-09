13af1ecf3ee92d20a352a858ac5fa8f1f4ef6c0b
Ravi Parmar <ravip@keypress.co.in>
2025-01-09 10:33:31 +0000

    testing

--- product_image_display_spt/__manifest__.py
+++ product_image_display_spt/__manifest__.py
@@ -2,7 +2,7 @@
 'version': '17.0.0.3',
 'summary': 'Summary\n',
 'sequence': 1,
-'price': 200.0,
+'price': 250.0,
 'currency': 'INR',
 'author': 'Keypress IT Services',
 'description': 'Description',
