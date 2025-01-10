ade2a981b11529d5e41cf44ee3304c93894d2c1b
Ravi Parmar <ravip@keypress.co.in>
2025-01-10 09:07:19 +0000

    New Release

--- kits_split_outstanding_credits/__manifest__.py
+++ kits_split_outstanding_credits/__manifest__.py
@@ -1,6 +1,6 @@
 {'name': 'Split Outstanding Credits',
 'sequence': 1,
-'version': '17.0.0.3',
+'version': '17.0.0.1',
 'category': 'Extra Tools',
 'author': 'Keypress IT Services',
 'website': 'https://staging.keypress.co.in',
@@ -14,6 +14,7 @@
 'auto_install': False,
 'depends': ['base', 'account'],
 'maintainer': 'Keypress IT Services',
-'currency': 'INR',
-'price': 610.0,
-'images': ['static/description/banner.png'],}
+'currency': 'USD',
+'price': 100.0,
+'images': ['static/description/banner.png'],
+'live_test_url': 'https://staging.keypress.co.in/odoo-apps/17.0/kits_split_outstanding_credits',}
