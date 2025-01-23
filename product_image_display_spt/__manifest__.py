207112441f745f3ce93ce6f880894da64c320892
Ravi Parmar <ravip@keypress.co.in>
2025-01-23 13:13:18 +0000

    testing

--- product_image_display_spt/__manifest__.py
+++ product_image_display_spt/__manifest__.py
@@ -1,5 +1,5 @@
 {'name': 'Odoo All In One Product Image Display',
-'version': '17.0.0.2',
+'version': '17.0.0.3',
 'summary': 'Odoo All In One Product Image Display',
 'sequence': 1,
 'images': ['static/description/banner.png'],
