0d5bcfa5fa2644e09dc8584613a8dc8fc5c59541
Ravi Parmar <ravip@keypress.co.in>
2025-01-25 04:31:51 +0000

    Change in manifest and documentation file.

--- product_image_display_spt/__manifest__.py
+++ product_image_display_spt/__manifest__.py
@@ -3,7 +3,7 @@
 'summary': 'Odoo All In One Product Image Display',
 'sequence': 1,
 'images': ['static/description/banner.png'],
-'price': 250.0,
+'price': 100.0,
 'currency': 'USD',
 'author': 'Keypress IT Services',
 'description': 'Odoo All In One Product Image Display',
